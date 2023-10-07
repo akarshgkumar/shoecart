55c5308832d1d13d2606d03bcae7d7d83d5b7f25
akarshgkumar <akarshgkumar2023@gmail.com>
2023-10-07 20:58:19 +0530

    jwt clear and isloggedin check added

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,6 +27,40 @@ function redirectIfLoggedIn(req, res, next) {
   }
 }
 
+async function setLoginStatus(req, res, next) {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      res.locals.isLoggedIn = false;
+      return next();
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const userEmail = decoded.email;
+
+    const user = await User.findOne({ email: userEmail });
+
+    if (user && user.verified) {
+      if(user.isBlocked) {
+        res.clearCookie('jwt');
+        res.locals.isLoggedIn = false;
+      } else {
+        res.locals.isLoggedIn = true;
+      }
+    } else {
+      res.locals.isLoggedIn = false;
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error in setLoginStatus middleware:", error);
+    res.locals.isLoggedIn = false;
+    next();
+  }
+}
+router.use(setLoginStatus);
+
+
 router.get("/login", noCache, redirectIfLoggedIn, (req, res) => {
   res.render("login");
 });
